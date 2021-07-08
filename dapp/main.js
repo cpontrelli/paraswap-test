b21529929b365893c3d21ec040264892d374ca11
Charlie Pontrelli <cpontrelli12@gmail.com>
2021-07-07 18:28:28 -0700

    Added approval for erc20 token swaps

--- dapp/main.js
+++ dapp/main.js
@@ -1,5 +1,31 @@
 const web3 = new Web3(Web3.givenProvider || "ws://localhost:8546");
 
+const tokenTransferProxy = "0xDb28dc14E5Eb60559844F6f900d23Dce35FcaE33";
+
+const erc20ABI = [{
+    "constant": false,
+    "inputs": [
+        {
+            "name": "_spender",
+            "type": "address"
+        },
+        {
+            "name": "_value",
+            "type": "uint256"
+        }
+    ],
+    "name": "approve",
+    "outputs": [
+        {
+            "name": "",
+            "type": "bool"
+        }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+}]
+
 const ETH = {
         symbol: "ETH",
         address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
@@ -7,6 +33,7 @@ const ETH = {
         img: "https://img.paraswap.network/ETH.png",
         network: 3
     }
+
 const DAI = {
         symbol: "DAI",
         address: "0xaD6D458402F60fD3Bd25163575031ACDce07538D",
@@ -15,7 +42,7 @@ const DAI = {
         network: 3
     }
 
-const amount = 10**17; // .1 ETH to exchange for DAI
+const amount = 100*10**18; // 1 DAI to exchange for ETH
 
 $(document).ready(function() {
     window.ethereum.request({ method: 'eth_requestAccounts' }).then(async function(accounts) {
@@ -26,7 +53,7 @@ $(document).ready(function() {
 });
 
 function getPriceRoute(account) {
-    const priceUrl = `https://apiv4.paraswap.io/v2/prices?network=3&from=${ETH.address}&to=${DAI.address}&amount=${amount}&side=SELL`;
+    const priceUrl = `https://apiv4.paraswap.io/v2/prices?network=3&from=${DAI.address}&to=${ETH.address}&amount=${amount}&side=SELL`;
 
     $.ajax({
         url: priceUrl,
@@ -44,7 +71,7 @@ function getPriceRoute(account) {
     });
 } 
 
-function exchange(account, priceRoute) {
+async function exchange(account, priceRoute) {
     const exchangeUrl = `https://apiv4.paraswap.io/v2/transactions/3`;
     
     //calculate minimum amount of DAI to recieve after 10% slippage
@@ -52,10 +79,10 @@ function exchange(account, priceRoute) {
     
     const config = {
         priceRoute,
-        srcToken: ETH.address,
-        srcDecimals: ETH.decimals,
-        destToken: DAI.address,
-        destDecimals: DAI.decimals,
+        srcToken: DAI.address,
+        srcDecimals: DAI.decimals,
+        destToken: ETH.address,
+        destDecimals: ETH.decimals,
         srcAmount: amount.toFixed(0),
         destAmount,
         userAddress: account,
@@ -63,23 +90,37 @@ function exchange(account, priceRoute) {
         receiver: '0x0000000000000000000000000000000000000000'
     }
 
-    $.ajax({
-        url: exchangeUrl,
-        type: "POST",
-        headers: {
-            'X-Partner':'LooneySwap',
-            'Content-Type': 'application/json'
-        },
-        data: JSON.stringify(config),
-        success: function(tx){
-            web3.eth.sendTransaction(tx, (err) => {
-                console.log(err);
-            });
-        },
-        error: function(error){
-            console.log('Error getting transaction');
-            console.log(error);
-        }
-    })
+    let approved = await approveToken(config.srcToken, tokenTransferProxy, account, config.srcAmount)
 
+    if (approved) {
+        $.ajax({
+            url: exchangeUrl,
+            type: "POST",
+            headers: {
+                'X-Partner':'LooneySwap',
+                'Content-Type': 'application/json'
+            },
+            data: JSON.stringify(config),
+            success: function(tx){
+                web3.eth.sendTransaction(tx, (err) => {
+                    console.log(err);
+                });
+            },
+            error: function(error){
+                console.log('Error getting transaction');
+                console.log(error);
+            }
+        })
+    } else {
+        console.log('Allowance not approved')
+    }
+}
+
+async function approveToken(tokenAddress, contractAddress, account, amount) {
+    if (tokenAddress != "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE") {
+        contractInstance = new web3.eth.Contract(erc20ABI, tokenAddress, {from: account});
+        return await contractInstance.methods.approve(contractAddress, amount).send();
+    } else {
+        return true;
+    } 
 }
